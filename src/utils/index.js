708d9f397809102f7d519f28225b606d0381149e
Broda Noel <brodanoel@gmail.com>
2017-12-16 21:46:26 -0300

    Add ZeroFill util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,15 @@
 import queryString from 'query-string';
 
 export default {
-  getQueryString: (location) => queryString.parse(location.search)
+  getQueryString: (location) => queryString.parse(location.search),
+
+  /**
+   * ZeroFill behavior in Javascript.
+   * Taken form https://stackoverflow.com/questions/5366849/convert-1-to-0001-in-javascript
+   */
+  zeroFill: (number) => {
+    const str = '' + number;
+    let pad = '00';
+    return pad.substring(0, pad.length - str.length) + str;
+  }
 };

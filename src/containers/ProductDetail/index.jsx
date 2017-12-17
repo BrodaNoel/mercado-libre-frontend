b9f3fa282f9f29a4808c2867eeff06a7e1cf503a
Broda Noel <brodanoel@gmail.com>
2017-12-17 15:49:22 -0300

    Improve Product Details

--- src/containers/ProductDetail/index.jsx
+++ src/containers/ProductDetail/index.jsx
@@ -9,32 +9,40 @@ class ProductDetail extends Component {
 
     return (
       <div className="ProductDetail">
-        <div className="picture">
-          <img src={product.picture} alt="product"/>
+        <div className="breadcrumbs">
+          <ul>
+            { product.categories.map((category, i) => (<li key={i}>{category}</li>)) }
+          </ul>
         </div>
 
-        <div className="details">
-          <div className="condition">
-            {product.condition === 'new' ? 'Nuevo' : 'Usado' } - {product.sold_quantity} vendidos
+        <div className="product">
+          <div className="picture">
+            <img src={product.picture} alt="product"/>
           </div>
 
-          <div className="title">{product.title}</div>
-
-          <div>
-            <div className="price">
-              $ {utils.parseCurrency(product.price.amount)}
-              <span className="decimals">{utils.zeroFill(product.price.decimals)}</span>
+          <div className="details">
+            <div className="condition">
+              {product.condition === 'new' ? 'Nuevo' : 'Usado' } - {product.sold_quantity} vendidos
             </div>
 
-            <i className="freeShipping"></i>
+            <div className="title">{product.title}</div>
+
+            <div>
+              <div className="price">
+                $ {utils.parseCurrency(product.price.amount)}
+                <span className="decimals">{utils.zeroFill(product.price.decimals)}</span>
+              </div>
+
+              <i className="freeShipping"></i>
+            </div>
+
+            <button>Comprar</button>
           </div>
 
-          <button>Comprar</button>
-        </div>
-
-        <div className="description">
-          <h3>Descripción del producto</h3>
-          {product.description}
+          <div className="description">
+            <h3>Descripción del producto</h3>
+            {product.description}
+          </div>
         </div>
       </div>
     );

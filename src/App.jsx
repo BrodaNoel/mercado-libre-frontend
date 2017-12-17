a01a6147e039e413fc71b845581ef34c2ef09224
Broda Noel <brodanoel@gmail.com>
2017-12-16 21:47:56 -0300

    Implement App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from 'pages/Home';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        In progress...
+        <Switch>
+          {
+            // Here we are not taking advantage of Routing, but, in a real
+            // scenario, these Route will give us the possibility of switch
+            // pages depending the URL.
+          }
+          <Route path='/items/:id' component={ Home } />
+          <Route path='/items' component={ Home } />
+          <Route path='/' component={ Home } />
+        </Switch>
       </div>
     );
   }

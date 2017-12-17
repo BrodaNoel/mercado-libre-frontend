4c15be08a33b9e1a1bae5829df24777e430db41a
Broda Noel <brodanoel@gmail.com>
2017-12-16 22:09:56 -0300

    Add CurrencyFOrmater util

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,11 +5,17 @@ export default {
 
   /**
    * ZeroFill behavior in Javascript.
-   * Taken form https://stackoverflow.com/questions/5366849/convert-1-to-0001-in-javascript
+   * Taken from https://stackoverflow.com/questions/5366849/convert-1-to-0001-in-javascript
    */
   zeroFill: (number) => {
     const str = '' + number;
     let pad = '00';
     return pad.substring(0, pad.length - str.length) + str;
-  }
+  },
+
+  /**
+   * Currency formater.
+   * Taken from https://stackoverflow.com/questions/149055/how-can-i-format-numbers-as-dollars-currency-string-in-javascript
+   */
+  parseCurrency: (number) => Number(number.toFixed(0)).toLocaleString('es')
 };
